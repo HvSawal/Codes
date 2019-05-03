4fd2f6f48e2013581063b035e0727dd3616b093f
Harshvardhan Sawal <hv.sawal@gmail.com>
2019-05-03 01:57:58 -0400

    Initial commit

---
+++ 1. Data Structures/1. Arrays/arrays.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void printArray(int* array, int len){
+    for(int i = 0; i < len; i++){
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void printVector(vector<int> array){
+    for(int i = 0; i < array.size(); i++){
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    /*Learning to Implement Arrays in different ways
+    *1. Static Implementaion
+    *2. Dynamic Implementation (STL)
+    *3. Dynamic Implementation (Pointers)
+    */
+
+    //Static Implementation
+    //=======================================================================================
+    int static_1[5]; //This initializes an empty array of size 5
+    len = sizeof(static_1)/sizeof(static_1[0]);
+    for(int i = 0; i < len; i++) static_1[i]=i;
+    printArray(static_1,len);
+
+    int static_2[] = {1, 2, 3, 4, 5}; //Creates an array of size 5 with the given values
+    len = sizeof(static_2)/sizeof(static_2[0]);
+    printArray(static_2,len);
+    //=======================================================================================
+
+    //Dynamic Implementation (STL)
+    //=======================================================================================
+    vector<int> dynamic_array;  //This initializes an empty array of size 5
+    for(int i=0;i<10;i++){
+        dynamic_array.push_back(i);
+    }
+    printVector(dynamic_array);
+
+    //=======================================================================================
+
+    return 0;
+}
