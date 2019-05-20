cdf79f1c040e968cfd0d5523320ff8a662399177
Harshvardhan Sawal <hv.sawal@gmail.com>
2019-05-20 01:45:17 -0400

    udpated

--- 1. Data Structures/1_Arrays/arrays_fuctions.cpp
+++ 1. Data Structures/1_Arrays/arrays_fuctions.cpp
@@ -14,9 +14,12 @@ void printVector(vector<int> array){
 int main(){
 
     vector<int> dynamic_array;  //This initializes an empty array of size 5
+    //vector<int>::iterator it;
     for(int i=0;i<10;i++){
-        dynamic_array.push_back(i);     //push_back is used to insert values at the end of the vector.
+        dynamic_array.push_back(i+1);     //push_back is used to insert values at the end of the vector.
     }//running time of push_back is anortized to O(1) as it appends to the end of vector, in simple terms
     cout<<"Vector size: "<<dynamic_array.size()<<endl;
+    cout<<"Is the vector empty? : "<<dynamic_array.empty()<<endl;
+    auto it = dynamic_array.insert(dynamic_array.begin()+3, 54); //iterate over till 4th index and insert the new element. running time is O(n)
     printVector(dynamic_array);
 }
