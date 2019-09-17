8595b04262ce8ccf88b230fb18a91e1d5e90a031
Harshvardhan Sawal <hv.sawal@gmail.com>
2019-09-16 20:52:28 -0400

    Code updated, final

--- 4. Cracking the Coding Interview/findCommon.cpp
+++ 4. Cracking the Coding Interview/findCommon.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
@@ -60,15 +61,48 @@ int findCommon3(vector<int> A, vector<int> B){
     int count = 0;
     int n = A.size();
 
+    unordered_map<int, int> map;
+
+    for(int i=0;i<n;i++){
+        map[A[i]]++;
+    }
+    for(int i=0;i<n;i++){
+        if(map.find(B[i]) != map.end()){
+            count++;
+        }
+    }
     return count;
 }
 
 //Time: N
 //Space: 1
+int findCommon4(vector<int> A, vector<int> B){
+
+    int count = 0;
+    int n = A.size();
+    int l=0,r=0;
+    while(l<n){
+        if(A[l]==B[r]){
+            count++; 
+            l++;
+            r++;
+        }
+        else if(A[l]>B[r]){
+            r++;
+        }
+        else if(A[l]<B[r]){
+            l++;
+        }
+    }
+    return count;
+}
+
+
+
 int main(){
 
-    vector<int> A = {};
-    vector<int> B = {};
+    vector<int> A = {13, 27, 35, 40, 49, 55, 59};
+    vector<int> B = {17, 35, 39, 40, 55, 58, 60};
 
     int count = findCommon(A,B);
 
