664be334c6daef877311ffaf8c3e0a09c92a6937
Harshvardhan Sawal <hv.sawal@gmail.com>
2020-05-23 22:31:38 -0400

    Level 3 Challenge 2 Solution added

---
+++ 5. Google Foobar Challenge/Level 3/Challenge 2 - Fuel_Injection_Perfection/solution.py
@@ -0,0 +1,24 @@
+#Least Significant Bit
+def solution(S):
+  n = int(S)
+  count = 0
+  #Check for LSB (least significant bit)
+  #in the binary rep of the number
+  while n>1:
+    #if LSB is *0
+    if n%2==0:
+      n=n//2
+    
+    #if end bits 01
+    elif n%4==1:
+      n-=1
+    elif n==3:
+      n-=1
+    
+    #if end bits 11
+    else:
+      n+=1
+    count+=1
+  return count;
+
+print(solution('14'))
